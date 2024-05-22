9ff85f516ec903c7d65b827b3dc86d0803732f5f
natdm02 <natalidimarino2002@gmail.com>
2024-05-22 16:45:41 +0200

    [add] migration and seeder

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,24 +1,26 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        <h3 class="text-center mt-3">Dashboard</h3>
-        <div class="row justify-content-center">
-            <div class="col">
-                <div class="card">
-                    <div class="card-header">{{ __('User Dashboard') }}</div>
+<div class="container">
+    <h2 class="fs-4 text-secondary my-4">
+        <h3>Projects</h3>
+    </h2>
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">{{ __('User Dashboard') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                        @endif
-
-                        <span class="fw-bold">{{ Auth::user()->name }}</span> {{ __('sei loggato/a! Bravo/a') }}
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
                     </div>
+                    @endif
+
+                    <span class="fw-bold">{{ Auth::user()->name }}</span> {{ __('sei loggato/a! Bravo/a') }}
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
